3664a7cddd4c9849c10364ff21b93ddad4df4a1f
DIEGO CASTRO <91438922+castrodiegojose@users.noreply.github.com>
2023-07-05 14:53:37 -0500

    Delete build directory

--- build/routes/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pointsController_1 = require("../controllers/pointsController");
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get('/points', pointsController_1.getAllPoints);
-exports.default = routes;
-//# sourceMappingURL=routes.js.map
